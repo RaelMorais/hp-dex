772fe088651324eb796df1399010efc8b9e4c8d7
RaelMorais <israelsantanamorais@outlook.com>
2025-06-24 08:31:23 -0300

    funcionou???

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { RoutesReact } from "./RoutesReact";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
         <RoutesReact/>
-      </BrowserRouter>
     </>
   )
 }
--- src/RoutesReact.jsx
+++ src/RoutesReact.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 export function RoutesReact(){
     return(
 
         <>
-        <BrowserRouter basename="/hp-dex">
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
             </Routes>
-        </BrowserRouter>
         </>
     );
 }
